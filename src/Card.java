afef2636a080f72e9e1b4c327508ff19ea667b7a
red-Wolf07 <jasonhicks012@gmail.com>
2026-05-01 10:38:24 -0400

    Started Card class

---
+++ src/Card.java
@@ -0,0 +1,19 @@
+
+/**
+ * Card Class
+ *
+ * @author (Jason Hicks)
+ * @version (v1.0, 05/01/2026)
+ */
+public class Card extends Sprite2D{
+
+    public final int HEARTS = 1;
+    public final int SPADES = 2;
+    public final int DIAMONDS = 3;
+    public final int CLUBS = 4;
+
+    public Card()
+    {
+        super(0, 0, 0);
+    }
+}
